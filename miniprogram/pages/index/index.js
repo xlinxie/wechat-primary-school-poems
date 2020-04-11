544b90848ccf4b83f689b8b84ddc1f1181b1b028
lawrence.lau <imdragonet@gmail.com>
2020-04-11 20:36:17 +0800

    chore: initiated with index page

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,35 @@
+//index.js
+Page({
+  data: {
+    grades: [
+      {
+          title: '一年级',
+          cover: '/public/images/cover/1.png'
+      },
+      {
+          title: '二年级',
+          cover: '/public/images/cover/2.png'
+      },
+      {
+          title: '三年级',
+          cover: '/public/images/cover/3.png'
+      },
+      {
+          title: '四年级',
+          cover: '/public/images/cover/4.png'
+      },
+      {
+          title: '五年级',
+          cover: '/public/images/cover/5.png'
+      },
+      {
+          title: '六年级',
+          cover: '/public/images/cover/6.png'
+      },
+    ]
+  },
+
+  onLoad: function() {
+
+  },
+})
